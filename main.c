0a9096c6f2c5a8396007e6e9cd59144fb223020e
Wojciech Marciniak <nnown1@gmail.com>
2019-11-06 07:01:37 +0100

    Switched to UI

--- main.c
+++ main.c
@@ -1,28 +1,11 @@
-// Projekt 1
-// Program przeliczający temperatury
-
-// Program po odpaleniu wyświetla menu, użytkownik wybira co chce zrobić, a po zakończeniu zadania program wraca zawsze do menu (chyba że użytkownik wybierze opcje zamknięcia programu).
-
-// W menu znajdują się przynajmniej następujące elementy:
-// * 1-6 funkcje przeliczania temperatur / dodają nowe wiersze
-// * wyświelt dane historyczne
-// * wypełnij wiersz losowymi wartościami
-// * wypełnij tablice (n) wierszy losowymi wartościami
-// * zmodyfikuj/podmień dane w wierszu
-// * usuń wybrany wiersz
-// * usuń wszystkie dane w tabeli
-// * wyświetl dane dla podanego zakresu wierszy
-// * zakończ program
-
-
-// Program przechowuje dane o przeszłych przeliczeniach temperatur, które mogą zostać wyświetlone (np. numer wiersza, temperatura w C, temperatura w K, temperatura w F). Dane wprowadzone przez użytkownika są zapisywane w takiej tabeli, a puste wartości są automatycznie uzupełniane. Program posiada zabezpieczenia przez niewłaściwym wpisaniem przez użytkownika wartości (np. gdy program spodziewa się wartości int, a dostaje char albo jeśli użytkownik wprowadzi ujemne Kelviny). Program za każdym razem pokazuje użytkownikowi co dokładnie powienien wprowadzić. Usuwanie wierszy: przed usunięciem pyta o to czy użytkonik na pewno chce to zrobić, a jeśli wiersza nie ma pyta raz jeszcze o numer wiersza (a co jeśli użytkonik będzie wprowadzał same błedne numery wierszy?)
-
-// Nie można podmienić wiersza na taki sam (wartości musą być inne). Wyświetlanie zakresu wierszy działa dla poprawnych zakresów wierszy np. od 4 do 8 (przy czym wpisanie 8 i 4 wyświetli wiersz 8, 7, 6, 5 i 4 w tej kolejności) Podmiana wiersza wyświetla całą tabelę i pyta, który wiersz zmodyfikować.
-
-// Dla chętnych: dodanie zapisu i wyczytwania tabelki z pliku.
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <windows.h>
+#include <conio.h>
+
+#define TEMPLATE_HEIGHT 6
+#define TEMPLATE_WIDTH 32
 
 float CelToFarh(float C);
 float CelToKel(float C);
@@ -30,56 +13,154 @@ float FarhToCel(float F);
 float FarhToKel(float F);
 float KelToCel(float K);
 float KelToFarh(float K);
-bool isTempValid(int tempUnit, float temp);
 
+char menuTemplate[TEMPLATE_HEIGHT][TEMPLATE_WIDTH] = {
+    "------------------------------\n\r",
+    "|                            |\n\r",
+    "|                            |\n\r",
+    "|                            |\n\r",
+    "------------------------------\n\r",
+    "        C( ) F( ) K( )        \n\r",
+};
+
+char menuUi[TEMPLATE_HEIGHT][TEMPLATE_WIDTH];
+char *displayPointer[TEMPLATE_WIDTH - 4];
+char *tempUnitPointer[3];
+
+void renderTemplate(char template[TEMPLATE_HEIGHT][TEMPLATE_WIDTH], int row, int col, int colour);
+void initializeUi(char template[TEMPLATE_HEIGHT][TEMPLATE_WIDTH], char ui[TEMPLATE_HEIGHT][TEMPLATE_WIDTH]);
+void hideCursor();
+
+enum ASCIIChars {BACKSPACE = 8, ESCAPE = 27, SPACE = 32, ASTERISK = 42, MINUS_SIGN = 45, DOT = 46, ZERO = 48, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, ARROW_UP = 72, ARROW_LEFT = 75, ARROW_RIGHT = 77, ARROW_DOWN = 80, RESTART = 114};
+enum TempUnits {CEL, FARH, KEL};
+enum Buttons {CEL_BTN = 10, FARH_BTN = 15, KEL_BTN = 20};
+
+typedef struct consoleInformation {
+    char sign;
+    int currentRow;
+    int currentCol;
+    int currentColour; //Hex value
+    enum TempUnits currentTempUnit;
+    enum Buttons currentButton;
+    float currentTempValue;
+} consoleInformation;
+
+
+void addNumberToDisplay(char *pointer[], char number);
+void deleteNumberFromDisplay(char *pointer[]);
+
+bool setFloatingPoint(char *pointer[]);
+void setSign(char *pointer[], char sign);
+
+float getNumberFromDisplay(char *pointer[]);
+void printNumberInDisplay(char *pointer[], float number);
+
+void updateTemperaturUnitInDisplay(char *pointer[], int currentTempUnit);
+void convertTemperatureToAnotherUnit(char *pointer[], char *displayPointer[], enum TempUnits *currentTempUnit, enum Buttons nextTempUnit, float *currentTempValue);
 
 int main() {
-    while(1) {
-        int choosenMenuOption;
-        float tempInput;
-        printf("Wybierz opcje:\n\r1.Celsjusz na Farenheit\n\r2.Celsjusz na Kelvin\n\r3.Farenheit na Celsjusz\n\r4.Farenheit na Kelvin\n\r5.Kelvin na Celsjusz\n\r6.Kelvin na Farenheit\n\r7.Zakoncz program\n\r");
-        scanf("%d", &choosenMenuOption);
-        fflush(stdin);
-        
-        if(choosenMenuOption < 1 || choosenMenuOption > 7) {
-            printf("Została wybrana opcja spoza menu\n\r");
-            continue;
-        }
+    consoleInformation consoleInfo = {32, 5, 10, 0x0070, CEL, 0};
+    enum ASCIIChars currentPressedButton;
+    int i;
 
-        if(choosenMenuOption == 7) {
-            break;
-        }
+    initializeUi(menuTemplate, menuUi);
 
-        printf("Wprowadz Temperature: ");
-        scanf("%f", &tempInput);
-        printf("\n\r");
+    for(i = 0; i < TEMPLATE_WIDTH - 4; i++) {
+        displayPointer[i] = &menuUi[2][i + 1];
+    }
+    tempUnitPointer[0] = &menuUi[5][10];
+    tempUnitPointer[1] = &menuUi[5][15];
+    tempUnitPointer[2] = &menuUi[5][20];
 
-        if(!isTempValid(choosenMenuOption, tempInput)) {
-            printf("Wprowadzona zostala zla temperatura\n\r");
-            continue;
-        } else {
-            switch(choosenMenuOption) {
-                case 1: 
-                    printf("%f\n\r", CelToFarh(tempInput));
+    hideCursor();
+    renderTemplate(menuUi, consoleInfo.currentRow, consoleInfo.currentCol, consoleInfo.currentColour);
+    
+    while(currentPressedButton != ESCAPE) {
+        currentPressedButton = _getch();
+
+        if(currentPressedButton >= ZERO && currentPressedButton <= NINE) {
+            for(i = ZERO; i <= NINE; i++) {
+                if(currentPressedButton == i) {
+                    addNumberToDisplay(displayPointer, currentPressedButton);
                     break;
-                case 2: 
-                    printf("%f\n\r", CelToKel(tempInput));
-                    break;
-                case 3: 
-                    printf("%f\n\r", FarhToCel(tempInput));
-                    break;
-                case 4: 
-                    printf("%f\n\r", FarhToKel(tempInput));
-                    break;
-                case 5: 
-                    printf("%f\n\r", KelToCel(tempInput));
-                    break;
-                case 6: 
-                    printf("%f\n\r", KelToFarh(tempInput));
-                    break;
+                }
             }
         }
+
+        switch(currentPressedButton) {
+            case RESTART:
+                initializeUi(menuTemplate, menuUi);
+                consoleInfo.sign = SPACE;
+                break;
+            case BACKSPACE:
+                deleteNumberFromDisplay(displayPointer);
+                break;
+            case MINUS_SIGN:
+                if(consoleInfo.sign == SPACE) consoleInfo.sign = MINUS_SIGN;
+                else if(consoleInfo.sign == MINUS_SIGN) consoleInfo.sign = SPACE;
+                break;
+            case DOT:
+                if(setFloatingPoint(displayPointer)) {
+                    if(*displayPointer[TEMPLATE_WIDTH - 5] == 32) {
+                        *displayPointer[TEMPLATE_WIDTH - 6] = ZERO;
+                        addNumberToDisplay(displayPointer, DOT);
+                        break;
+                    } else {
+                        addNumberToDisplay(displayPointer, DOT);
+                        break;
+                    }
+                }
+            case ARROW_LEFT:
+                if(consoleInfo.currentCol == 10) {
+                    consoleInfo.currentCol = 20;
+                    consoleInfo.currentButton = KEL_BTN;
+                }
+                else if(consoleInfo.currentCol == 15) {
+                    consoleInfo.currentCol = 10;
+                    consoleInfo.currentButton = CEL_BTN;
+                }
+                else if(consoleInfo.currentCol == 20) {
+                    consoleInfo.currentCol = 15;
+                    consoleInfo.currentButton = FARH_BTN;
+                }
+                break;
+            case ARROW_RIGHT:
+                if(consoleInfo.currentCol == 10) {
+                    consoleInfo.currentCol = 15;
+                    consoleInfo.currentButton = FARH_BTN;
+                }
+                else if(consoleInfo.currentCol == 15) {
+                    consoleInfo.currentCol = 20;
+                    consoleInfo.currentButton = KEL_BTN;
+                }
+                else if(consoleInfo.currentCol == 20) {
+                    consoleInfo.currentCol = 10;
+                    consoleInfo.currentButton = CEL_BTN;
+                }
+                break;
+            case SPACE:
+                if(consoleInfo.currentRow == 5) {
+                    convertTemperatureToAnotherUnit(tempUnitPointer, displayPointer, &consoleInfo.currentTempUnit, consoleInfo.currentButton, &consoleInfo.currentTempValue);
+                    if(*displayPointer[TEMPLATE_WIDTH - 5] != 32) {
+                        if(consoleInfo.currentTempValue < 0) {
+                            consoleInfo.sign = 45;
+                        } else {
+                            consoleInfo.sign = 32;
+                        }
+                        printNumberInDisplay(displayPointer, consoleInfo.currentTempValue);
+                    } 
+                }
+        }
+
+        setSign(displayPointer, consoleInfo.sign);
+
+        consoleInfo.currentTempValue = getNumberFromDisplay(displayPointer);
+        system("cls");
+        
+        updateTemperaturUnitInDisplay(tempUnitPointer, consoleInfo.currentTempUnit);
+        renderTemplate(menuUi, consoleInfo.currentRow, consoleInfo.currentCol, consoleInfo.currentColour);
     }
+
     return 0;
 }
 
@@ -107,9 +188,156 @@ float KelToFarh(float K) {
     return K * 9.0/5.0 - 459.67;
 }
 
-bool isTempValid(int tempUnit, float temp) {
-    if(((tempUnit == 1 || tempUnit == 2) && temp < -273.15) || ((tempUnit == 3 || tempUnit == 4) && temp < -459.67) || ((tempUnit == 5 || tempUnit == 6) && temp < 0)) {
-        return false;
+void renderTemplate(char template[TEMPLATE_HEIGHT][TEMPLATE_WIDTH], int row, int col, int colour) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
+    int i, j;
+    for(i = 0; i < TEMPLATE_HEIGHT; i++) {
+        for(j = 0; j < TEMPLATE_WIDTH; j++) {
+            if(i == row && j == col) {
+                SetConsoleTextAttribute(hConsole, colour);
+                putchar(template[i][j]);
+                SetConsoleTextAttribute(hConsole, saved_attributes);
+                continue;
+            }
+            putchar(template[i][j]);
+        }
+    }
+}
+
+void initializeUi(char template[TEMPLATE_HEIGHT][TEMPLATE_WIDTH], char ui[TEMPLATE_HEIGHT][TEMPLATE_WIDTH]) {
+    int i, j;
+    for(i = 0; i < TEMPLATE_HEIGHT; i++) {
+        for(j = 0; j < TEMPLATE_WIDTH; j++) {
+            ui[i][j] = template[i][j];
+        }
+    }
+}
+
+void hideCursor() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO consoleInfo;
+    consoleInfo.dwSize = 100;
+    consoleInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(hConsole, &consoleInfo);
+}
+
+void addNumberToDisplay(char *pointer[], char number) {
+    if(*pointer[1] != 32) return;
+
+    int i;
+    if(*pointer[TEMPLATE_WIDTH - 5] == 32) {
+        *pointer[TEMPLATE_WIDTH - 5] = number;
+    } else {
+        for(i = 1; i < TEMPLATE_WIDTH - 4; i++) {
+            if(*pointer[i] != 32) {
+                *pointer[i - 1] = *pointer[i];
+            }
+        }
+        *pointer[TEMPLATE_WIDTH - 5] = number;
+    }
+}
+
+void deleteNumberFromDisplay(char *pointer[]) {
+    if(*pointer[TEMPLATE_WIDTH - 5] == 32) return;
+
+    int i;
+    for(i = TEMPLATE_WIDTH - 6; i > 0; i--) {
+        *pointer[i + 1] = *pointer[i];
+    }
+    *pointer[1] = 32;
+}
+
+bool setFloatingPoint(char *pointer[]) {
+    int i;
+    for(i = 1; i < TEMPLATE_WIDTH - 4; i++) {
+        if(*pointer[i] == DOT) return false;
     }
     return true;
+}
+
+void setSign(char *pointer[], char sign) {
+    *pointer[0] = sign;
+}
+
+float getNumberFromDisplay(char *pointer[]) {
+    char sign = *pointer[0];
+    char number[TEMPLATE_WIDTH - 4];
+
+    int i, j;
+    for(i = 0; i < TEMPLATE_WIDTH - 4; i++) number[i] = 0;
+    
+    for(i = 0; i < TEMPLATE_WIDTH - 4; i++) {
+        if(*pointer[i] != 32) {
+            for(j = 0; j < TEMPLATE_WIDTH - 4; j++) {
+                if(number[j] == 0) {
+                    number[j] = *pointer[i];
+                    break;
+                }
+            }
+        }
+    }
+
+    return strtof(number, NULL);
+}
+
+void printNumberInDisplay(char *pointer[], float number) {
+    char numberAsAString[TEMPLATE_WIDTH - 4];
+    int i, j;
+
+    for(i = 0; i < TEMPLATE_WIDTH - 4; i++) {
+        numberAsAString[i] = 32;
+        *pointer[i] = 32;
+    }
+
+    sprintf(numberAsAString, "%f", number);
+
+    for(i = TEMPLATE_WIDTH - 5; i > 0; i--) {
+        if(numberAsAString[i] >= ZERO && numberAsAString[i] <= NINE) {
+            break;
+        }
+    }
+
+    for(i, j = TEMPLATE_WIDTH - 5; i > 0, j > 0; i--, j--) {
+        if(numberAsAString[i] == MINUS_SIGN) {
+            break;
+        }
+        
+        *pointer[j] = numberAsAString[i];
+    }
+}
+
+void updateTemperaturUnitInDisplay(char *pointer[], int currentTempUnit) {
+    *pointer[currentTempUnit] = ASTERISK;
+}
+
+void convertTemperatureToAnotherUnit(char *pointer[], char *displayPointer[], enum TempUnits *currentTempUnit, enum Buttons nextTempUnit, float *currentTempValue) {
+    if(*displayPointer[TEMPLATE_WIDTH - 5] != 32) {
+        switch(*currentTempUnit) {
+            case CEL: 
+                if(nextTempUnit == FARH_BTN) *currentTempValue = CelToFarh(*currentTempValue);
+                else if(nextTempUnit == KEL_BTN) *currentTempValue = CelToKel(*currentTempValue);
+            case FARH:
+                if(nextTempUnit == CEL_BTN) *currentTempValue = FarhToCel(*currentTempValue);
+                else if(nextTempUnit == KEL_BTN) *currentTempValue = FarhToKel(*currentTempValue);
+            case KEL:
+                if(nextTempUnit == CEL_BTN) *currentTempValue = KelToCel(*currentTempValue);
+                else if(nextTempUnit == FARH_BTN) *currentTempValue = KelToFarh(*currentTempValue);
+        }
+    }
+
+    *pointer[*currentTempUnit] = SPACE;
+
+    if(nextTempUnit == CEL_BTN) *currentTempUnit = CEL;
+    else if(nextTempUnit == FARH_BTN) *currentTempUnit = FARH;
+    else if(nextTempUnit == KEL_BTN) *currentTempUnit = KEL;
+
+    if(*currentTempUnit == CEL && *currentTempValue < -273.15) *currentTempValue = -273.15;
+    else if(*currentTempUnit == FARH && *currentTempValue < -459.67) *currentTempValue = -459.67;
+    else if(*currentTempUnit == KEL && *currentTempValue < 0) *currentTempValue = 0;
 }
